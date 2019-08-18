ef2fe52e8e30a8a428d81ce68d20f8605dd772ca
Kah Wai LIM <kahwai222@gmail.com>
2019-08-18 12:54:59 +0800

    End of Section 10

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { incrementCounter, decrementCounter } from './testActions';
 import { Button } from 'semantic-ui-react';
+import TestPlaceInput from './TestPlaceInput';
+import SimpleMap from './SimpleMap';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 const mapStateToProps = state => ({
   data: state.test.data
@@ -13,6 +16,28 @@ const actions = {
 };
 
 class TestComponent extends Component {
+  state = {
+    latlng: {
+      lat: 59.95,
+      lng: 30.33
+    }
+  };
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => {
+        this.setState({
+          latlng: latLng
+        });
+      })
+      .catch(error => console.error('Error', error));
+  };
+
+  setLatLng = (lat, lng) => {
+    this.setState({ lat, lng });
+  };
+
   render() {
     const { data, incrementCounter, decrementCounter } = this.props;
     return (
@@ -21,6 +46,10 @@ class TestComponent extends Component {
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} positive content="Increment" />
         <Button onClick={decrementCounter} negative content="Decrement" />
+        <br />
+        <br />
+        <TestPlaceInput selectAddress={this.handleSelect} />
+        <SimpleMap key={this.state.latlng.lng} latlng={this.state.latlng} />
       </div>
     );
   }
